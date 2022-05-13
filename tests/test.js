f4ec2d2e191aa674fdca65900693df40f51f2574
stcucufa <githubnyou@romulusetrem.us>
2022-05-13 11:56:41 +0200

    Manual tick test

--- tests/test.js
+++ tests/test.js
@@ -1,4 +1,4 @@
-import { isAsync } from "../lib/util.js";
+import { create, isAsync } from "../lib/util.js";
 
 const TestTimeoutMS = 3000;
 
@@ -20,13 +20,47 @@ export function test(title, f) {
     tests.push([title, f]);
 }
 
+const Test = {
+    create: create({
+        failures: []
+    }),
+
+    expect(p, message) {
+        if (!p) {
+            this.failures.push(message);
+        }
+    },
+
+    above(value, expected) {
+        this.expect(
+            value > expected,
+            `expected ${value} to be above (>) ${expected}`
+        );
+    },
+
+    atLeast(value, expected) {
+        this.expect(
+            value >= expected,
+            `expected ${value} to be at least (>=) ${expected}`
+        );
+    },
+
+    equal(value, expected) {
+        this.expect(
+            value === expected,
+            `expected ${value} to be equal to (===) ${expected}`
+        );
+    }
+};
+
 async function run() {
     for (const [title, test] of tests) {
         try {
+            const t = Test.create();
             if (isAsync(test)) {
-                await test();
+                await test(t);
             } else {
-                const promise = test();
+                const promise = test(t);
                 if (typeof promise?.then === "function") {
                     await Promise.race([
                         promise,
@@ -38,14 +72,21 @@ async function run() {
                     ]);
                 }
             }
-            console.log(`Success: "${title}"`);
-            successes += 1;
+            if (t.failures.length > 0) {
+                for (const failure of t.failures) {
+                    console.log(`Failure: "${title}" (${failure})`);
+                }
+                failures += 1;
+            } else {
+                console.log(`Success: "${title}"`);
+                successes += 1;
+            }
         } catch (error) {
             if (error.timeout) {
-                console.log(`Timeout: "${title}"`);
+                console.error(`Timeout: "${title}"`);
                 timeouts += 1;
             } else {
-                console.log(`Failure: "${title}"`, error);
+                console.error(`Failure: "${title}"`, error);
                 failures += 1;
             }
         }
