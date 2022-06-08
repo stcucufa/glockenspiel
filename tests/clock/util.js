70c64006600dce5de3a4669ac96efa6ed1d74014
stcucufa <githubnyou@romulusetrem.us>
2022-06-08 22:07:30 +0200

    Manual Clock, extend
    Easily create a clock with a manual tick. Also add the `extend()` utility function to extend an object, with tests.

--- tests/clock/util.js
+++ tests/clock/util.js
@@ -1,5 +1,1 @@
-import { Clock } from "../../lib/clock.js";
-import { ManualTick } from "../../lib/tick.js";
-
-export const manualClock = () => Clock.create({ tick: ManualTick.create() });
 export const pendingUpdate = clock => !!clock.tick.request;
